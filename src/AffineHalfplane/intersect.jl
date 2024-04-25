b6e53c9e7268d970868354089f67c237bd34f043
Justus Springer <justusspringer@gmx.de>
2024-04-25 09:54:25 +0200

    some comments

--- src/AffineHalfplane/intersect.jl
+++ src/AffineHalfplane/intersect.jl
@@ -17,8 +17,25 @@ end
 function intersect_halfplanes(halfplanes :: Vector{<:AffineHalfplane{T}}) where {T <: Integer}
 
     n = length(halfplanes)
+
+    # sort the halfplanes, see the `isless` implementation of
+    # `AffineHalfplanes`: Halfplanes are sorted first by their angle and
+    # second by their distance from the origin
     sort!(halfplanes)
 
+    # boundedness check
+    # There are other ways in which the given halfplanes do not give
+    # a pounded polytope, for example if there are two parallel horizontal
+    # halfplanes. This is checked for later, when intersecting the upper
+    # with the lower halfplanes
+    !isempty(filter(H -> -2 < pseudo_angle(H) < 0, halfplanes)) || error("this polyhedra is unbounded")
+    !isempty(filter(H -> 0 < pseudo_angle(H) < 2, halfplanes)) || error("this polyhedra is unbounded")
+
+    # split up into lower and upper halfplanes. At the same time,
+    # discard any halfplanes that superset of other halfplanes and thus,
+    # redundant. Since halfplanes of the same angle are sorted by
+    # distance, we only keep the halfplane with the largest distance
+    # for every given angle.
     upper_halfplanes, lower_halfplanes = AffineHalfplane{T}[], AffineHalfplane{T}[]
     for i = 1 : n
         H1, H2 = halfplanes[i], halfplanes[mod(i+1, 1:n)]
@@ -28,9 +45,15 @@ function intersect_halfplanes(halfplanes :: Vector{<:AffineHalfplane{T}}) where 
         end
     end
 
+    # remove redundancies in upper and lower halfplanes respectively.
+    # This is basically the dual the graham scan algorithm.
     upper_halfplanes = _intersect_sorted_halfplanes(upper_halfplanes)
     lower_halfplanes = _intersect_sorted_halfplanes(lower_halfplanes)
 
+
+    # Now we need to intersect the upper halfplanes with the lower
+    # halfplanes. For this, we determine intesection points of the
+    # lines and check if it lies in all the halfplanes.
     found_right = false
     for i = 1 : length(upper_halfplanes), j = length(lower_halfplanes) : -1 : 1
         Hu, Hl = upper_halfplanes[i], lower_halfplanes[j]
