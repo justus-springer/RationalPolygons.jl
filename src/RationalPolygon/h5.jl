350d5fa2093d823340f6a1781d24f380c9edc836
Justus Springer <justusspringer@gmx.de>
2024-06-18 15:23:54 +0200

    add restore_hash_set, blockify readin of polygons

--- src/RationalPolygon/h5.jl
+++ src/RationalPolygon/h5.jl
@@ -10,30 +10,22 @@ function create_polygon_dataset(
     dspace = dataspace((0,),(-1,))
     dtype = datatype(NTuple{2N, T})
     dset = create_dataset(f, path, dtype, dspace; chunk = (chunk_size,), deflate)
-    write_attribute(dset, "count", 0)
-    write_attribute(dset, "rationality", k)
     return dset
 
 end
 
-function read_polygon_dataset(
+
+function read_polygon_dataset(k :: T,
         f :: Union{HDF5.File, HDF5.Group},
-        path :: String)
+        path :: String, I...) where {T <: Integer}
 
     dset = HDF5.open_dataset(f, path)
-    n = read_attribute(dset, "count")
-    k = read_attribute(dset, "rationality")
     dtype = HDF5.datatype(dset)
     tuple_type = HDF5.get_jl_type(dtype)
     N = length(fieldnames(tuple_type)) ÷ 2
-    T = eltype(tuple_type)
-    Ms = HDF5.generic_read(dset, dtype, SMatrix{2,N,T,2N}, 1:n)
+    Ps = [RationalPolygon(M, k) for M ∈ HDF5.generic_read(dset, dtype, SMatrix{2,N,T,2N}, I...)]
     close(dset)
 
-    Ps = RationalPolygon{T,N,2N}[]
-    for i = 1 : n
-        push!(Ps, RationalPolygon(Ms[i], T(k)))
-    end
     return Ps
 end
 
@@ -45,15 +37,15 @@ function write_polygon_dataset(
     isempty(Ps) && return
     k = rationality(first(Ps))
     if !haskey(f, path)
-        create_polygon_dataset(f, path, k, N; T)
+        dset = create_polygon_dataset(f, path, k, N; T)
+    else
+        dset = open_dataset(f, path)
     end
 
-    dset = open_dataset(f, path)
-    n = read_attribute(dset, "count")
+    n = length(dataspace(dset))
     HDF5.set_extent_dims(dset, (n+length(Ps),))
     data = [vertex_matrix(P).data for P ∈ Ps]
     dset[n+1 : n+length(Ps)] = data
-    attrs(dset)["count"] += length(Ps)
     close(dset)
 
 end
--- src/Subpolygons/HDFSubpolygonStorage.jl
+++ src/Subpolygons/HDFSubpolygonStorage.jl
@@ -1,4 +1,4 @@
-struct HDFSubpolygonStorage{T <: Integer} <: SubpolygonStorage{T}
+mutable struct HDFSubpolygonStorage{T <: Integer} <: SubpolygonStorage{T}
     hdf_path :: String
     hdf_group :: String
     hash_sets :: Dict{T,Set{UInt64}}
@@ -19,7 +19,7 @@ function initialize_hdf_subpolygon_storage(
     all(P -> rationality(P) == k, Ps) || error("all polygons must have the same rationality")
     all(P -> number_of_interior_lattice_points(P) == n, Ps) || error("all polygons must have the same number of interior lattice points")
 
-    f = h5open(st.hdf_path, "cw")
+    f = h5open(st.hdf_path, "cw"; swmr = true)
     if !haskey(f, st.hdf_group)
         g = create_group(f, st.hdf_group)
     else
@@ -37,7 +37,7 @@ function initialize_hdf_subpolygon_storage(
     for a = 3 : maximum(normalized_area.(Ps))
         st.hash_sets[a] = Set{UInt64}()
     end
-    
+
     for P ∈ Ps
         a = normalized_area(P)
         N = number_of_vertices(P)
@@ -48,14 +48,44 @@ function initialize_hdf_subpolygon_storage(
         push!(st.hash_sets[a], hash(P))
     end
 
+    close(f)
+
+end
+
+export restore_hash_set
+function restore_hash_set(st :: HDFSubpolygonStorage{T}) where {T <: Integer}
+
+    f = h5open(st.hdf_path, "r+"; swmr = true)
+    g = f[st.hdf_group]
+    k = read_attribute(g, "rationality")
+
+    st.hash_sets = Dict{T,Set{UInt64}}()
+    last_completed_area = read_attribute(g, "last_completed_area")
+    for a = 3 : last_completed_area - 1
+        st.hash_sets[a] = Set{UInt64}()
+    end
+
+    as = [parse(Int, s[2:end]) for s ∈ keys(g)]
+    filter!(a -> a < last_completed_area, as)
+    for a ∈ as
+        st.hash_sets[a] = Set{UInt64}()
+        for n ∈ keys(g["a$a"])
+            Ps = read_polygon_dataset(k, g["a$a"], n)
+            for P ∈ Ps
+                push!(st.hash_sets[a], hash(P))
+            end
+        end
+    end
+
 end
 
 
 function subpolygons_single_step(
         st :: HDFSubpolygonStorage{T};
-        logging :: Bool = false) where {T <: Integer}
+        logging :: Bool = false,
+        block_size :: Int = 10^6) where {T <: Integer}
 
-    f = h5open(st.hdf_path, "r+")
+    f = h5open(st.hdf_path, "r+"; swmr = true)
     g = f[st.hdf_group]
 
     k = read_attribute(g, "rationality")
@@ -77,71 +107,81 @@ function subpolygons_single_step(
     ns = sort([parse(Int, s[2:end]) for s ∈ keys(current_area_group)])
     for n ∈ ns
 
-        Ps = read_polygon_dataset(current_area_group, "n$n") 
+        N = length(dataspace(current_area_group["n$n"]))
+        number_of_blocks = N ÷ block_size + 1
 
-        out_dicts = Dict{Tuple{T,Int}}{Set{<:RationalPolygon{T}}}[]
-        for i = 1 : Threads.nthreads()
-            push!(out_dicts, Dict{Tuple{T,Int}}{Set{<:RationalPolygon{T}}}())
-        end
+        for b = 1 : number_of_blocks
 
-        logging && @info "[Area = $(current_area), #vertices = $n]. Polygons to peel: $(length(Ps))."
+            I = (b-1)*block_size + 1 : min(b*block_size, N)
 
-        Threads.@threads for P ∈ Ps
-            id = Threads.threadid()
-            for j = 1 : n
-                Q, keeps_genus = remove_vertex(P, j; primitive)
-                keeps_genus || continue
-                Qn = number_of_vertices(Q)
-                Qn > 2 || continue
-                if use_affine_normal_form
-                    Q = affine_normal_form(Q)
-                else
-                    Q = unimodular_normal_form(Q)
-                end
+            Ps = read_polygon_dataset(k, current_area_group, "n$n", I) 
 
-                Qa = normalized_area(Q)
-                if !haskey(out_dicts[id], (Qa,Qn))
-                    out_dicts[id][(Qa,Qn)] = Set{RationalPolygon{T,Qn,2*Qn}}()
-                end
-                Q ∉ out_dicts[id][(Qa,Qn)] || continue
-                hash(Q) ∉ st.hash_sets[Qa] || continue
-                push!(out_dicts[id][(Qa,Qn)], Q)
+            out_dicts = Dict{Tuple{T,Int}}{Set{<:RationalPolygon{T}}}[]
+            for i = 1 : Threads.nthreads()
+                push!(out_dicts, Dict{Tuple{T,Int}}{Set{<:RationalPolygon{T}}}())
             end
-        end
 
-        polygons_in_memory_count = sum([length(values(d)) for d ∈ out_dicts])
-        logging && @info "[Area = $(current_area), #vertices = $n]. Peeling complete. New polygons: $polygons_in_memory_count"
+            logging && @info "[a = $(current_area), n = $n, block $b/$number_of_blocks]. Polygons to peel: $(length(Ps))."
 
-        for i = 2 : length(out_dicts)
-            mergewith!(union!, out_dicts[1], out_dicts[i])
-            out_dicts[i] = Dict{Tuple{T,Int}}{Set{<:RationalPolygon{T}}}()
-        end
+            Threads.@threads for P ∈ Ps
+                id = Threads.threadid()
+                for j = 1 : n
+                    Q, keeps_genus = remove_vertex(P, j; primitive)
+                    keeps_genus || continue
+                    Qn = number_of_vertices(Q)
+                    Qn > 2 || continue
+                    if use_affine_normal_form
+                        Q = affine_normal_form(Q)
+                    else
+                        Q = unimodular_normal_form(Q)
+                    end
 
-        for ((Qa,Qn), Qs) ∈ out_dicts[1]
-            path = "a$(Qa)/n$(Qn)"
-            write_polygon_dataset(g, path, collect(Qs))
-            for Q ∈ Qs
-                push!(st.hash_sets[Qa], hash(Q))
+                    Qa = normalized_area(Q)
+                    if !haskey(out_dicts[id], (Qa,Qn))
+                        out_dicts[id][(Qa,Qn)] = Set{RationalPolygon{T,Qn,2*Qn}}()
+                    end
+                    Q ∉ out_dicts[id][(Qa,Qn)] || continue
+                    hash(Q) ∉ st.hash_sets[Qa] || continue
+                    push!(out_dicts[id][(Qa,Qn)], Q)
+                end
             end
-            attrs(g)["total_count"] += length(Qs)
+
+            new_polygons_count = sum([sum(length.(values(d))) for d ∈ out_dicts])
+            logging && @info "[a = $(current_area), n = $n]. Peeling complete. New polygons: $new_polygons_count."
+
+            for i = 2 : length(out_dicts)
+                mergewith!(union!, out_dicts[1], out_dicts[i])
+                out_dicts[i] = Dict{Tuple{T,Int}}{Set{<:RationalPolygon{T}}}()
+            end
+
+            for ((Qa,Qn), Qs) ∈ out_dicts[1]
+                path = "a$(Qa)/n$(Qn)"
+                write_polygon_dataset(g, path, collect(Qs))
+                for Q ∈ Qs
+                    push!(st.hash_sets[Qa], hash(Q))
+                end
+                attrs(g)["total_count"] += length(Qs)
+            end
+
+            total_count = read_attribute(g, "total_count")
+            hash_set_size = sum([length(Qs) for (_,Qs) ∈ st.hash_sets])
+            hash_set_bytes = Base.format_bytes(Base.summarysize(st.hash_sets))
+            
+            logging && @info "[a = $current_area, n = $n]. Writeout complete. Running total: $total_count. Hash set size: $hash_set_size ($hash_set_bytes)."
+
         end
 
     end
 
     attrs(g)["last_completed_area"] = current_area
     delete!(st.hash_sets, current_area)
-    hash_set_size = sum([length(Qs) for (_,Qs) ∈ st.hash_sets])
-    hash_set_bytes = Base.format_bytes(Base.summarysize(st.hash_sets))
-    total_count = read_attribute(g, "total_count")
-
-    logging && @info "[Area = $current_area]. Running total: $total_count. Hash set size: $hash_set_size ($hash_set_bytes)."
 
     close(f)
 
 end
 
 function is_finished(st :: HDFSubpolygonStorage{T}) where {T <: Integer}
-    f = h5open(st.hdf_path, "r")
+    f = h5open(st.hdf_path, "r"; swmr = true)
     is_finished = read_attribute(f[st.hdf_group], "is_finished")
     close(f)
     return is_finished
