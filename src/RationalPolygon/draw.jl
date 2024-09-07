205eccc32445ec13d38745966d2118b75c19e22f
Justus Springer <justusspringer@gmx.de>
2024-09-07 11:16:30 +0200

    make plot_polygons take a Vector

--- src/RationalPolygon/draw.jl
+++ src/RationalPolygon/draw.jl
@@ -1,12 +1,17 @@
-function plot_polygon(P :: RationalPolygon{T,N}) where {N, T <: Integer}
-    vs = vertices(P)
-    k = rationality(P)
-    shape = Shape([(v[1],v[2]) for v ∈ vs])
-    xmin = minimum([v[1] for v ∈ vs]) - 1//k
-    xmax = maximum([v[1] for v ∈ vs]) + 1//k
-    ymin = minimum([v[2] for v ∈ vs]) - 1//k
-    ymax = maximum([v[2] for v ∈ vs]) + 1//k
-    plot(shape, 
+function plot_polygon(Ps :: Vector{<:RationalPolygon{T}}) where {T <: Integer}
+    k = rationality(first(Ps))
+    all(P -> rationality(P) == k, Ps) || error("Polygons must have the same rationality")
+
+    xmin = minimum([v[1] for P ∈ Ps for v ∈ P]) - 1//k
+    xmax = maximum([v[1] for P ∈ Ps for v ∈ P]) + 1//k
+    ymin = minimum([v[2] for P ∈ Ps for v ∈ P]) - 1//k
+    ymax = maximum([v[2] for P ∈ Ps for v ∈ P]) + 1//k
+
+    lattice_points = [(x,y) for x = ceil(xmin) : floor(xmax) for y = ceil(ymin) : floor(ymax)]
+
+    plot(lattice_points,
+         seriestype = :scatter,
+         markercolor = :black,
          xlims = (xmin,xmax),
          ylims = (ymin,ymax),
          xticks = ceil(xmin):1:floor(xmax),
@@ -14,14 +19,16 @@ function plot_polygon(P :: RationalPolygon{T,N}) where {N, T <: Integer}
          aspect_ratio = :equal,
          axis = false,
          label = false,
-         grid = false,
-         fillcolor = plot_color(:gray, 0.3))
+         grid = false)
 
-    lattice_points = [(x,y) for x = ceil(xmin) : floor(xmax) for y = ceil(ymin) : floor(ymax)]
+    P = first(Ps)
+    for P ∈ Ps
+        vs = vertices(P)
+        k = rationality(P)
+        shape = Shape([(v[1],v[2]) for v ∈ vs])
+        plot!(shape, label = false, fillcolor = plot_color(:gray, 0.3), show = true)
+    end
 
-    plot!(lattice_points,
-          seriestype = :scatter,
-          markercolor = :black,
-          label = :false)
 end
 
+plot_polygon(P :: RationalPolygon{T}) where {T <: Integer} = plot_polygon([P])
