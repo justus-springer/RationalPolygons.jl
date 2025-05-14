306975796b888a01a0f843097e42e6774a2ee308
Justus Springer <justusspringer@gmx.de>
2025-05-14 15:27:46 +0200

    clean up and document

--- src/Classification/quadrilaterals.jl
+++ src/Classification/quadrilaterals.jl
@@ -36,6 +36,14 @@ end
 modified_unit_fraction_solutions(q :: Rational{T}, c :: T, d :: T) where {T <: Integer} =
 modified_unit_fraction_solutions(numerator(q), denominator(q), c, d)
 
+
+@doc raw"""
+    gorenstein_coefficients_to_degree_matrix_minors(ι :: T, A :: SMatrix{4,2,T,8}) where {T <: Integer}
+
+Compute all minors of the Fano ι-Gorenstein matrix Q associated with
+the Gorenstein coefficients A.
+
+"""
 function gorenstein_coefficients_to_degree_matrix_minors(ι :: T,
         A :: SMatrix{4,2,T,8}) where {T <: Integer}
 
@@ -56,10 +64,36 @@ function gorenstein_coefficients_to_degree_matrix_minors(ι :: T,
 
 end
 
+shift_gorenstein_coefficients(A :: SMatrix{4,2,T,8}, k :: Int) where {T <: Integer} =
+SMatrix{4,2,T,8}(A[mod(i+k, 1:4), j] for i = 1 : 4, j = 1 : 2)
 
-function get_gorenstein_coefficients(ι :: T) where {T <: Integer}
+reflect_gorenstein_coefficients(A :: SMatrix{4,2,T,8}) where {T <: Integer} =
+SMatrix{4,2,T,8}(A[mod(3-i, 1:4), mod(3-j, 1:2)] for i = 1 : 4, j = 1 : 2)
 
-    res = SMatrix{4,2,T}[]
+function gorenstein_coefficients_normal_form(A :: SMatrix{4,2,T,8}) where {T <: Integer}
+
+    As = SVector{8,SMatrix{4,2,T,8}}(reflect ?
+        reflect_gorenstein_coefficients(shift_gorenstein_coefficients(A,i)) :
+        shift_gorenstein_coefficients(A,i)
+    for i = 1 : 4, reflect ∈ [true,false])
+
+    return argmin(vec, As)
+
+end
+
+@doc raw"""
+    classify_gorenstein_coefficients(ι :: T, ::Val{t})
+    classify_gorenstein_coefficients(ι :: T)
+
+Classify Gorenstein coefficients associated to Fano ι-Gorenstein matrices.
+Optionally takes in a `Val{t}` argument, where `t` can be 1, 2, 3 or 4.
+In this case, only the Gorenstein coefficients of type `t` are classified.
+(TODO: Add reference to dissertation/paper for definition of types 1-4).
+
+"""
+function classify_gorenstein_coefficients(ι :: T, ::Val{1}) where {T <: Integer}
+
+    res = Set{SMatrix{4,2,T}}()
 
     # Case a11 < a12
     for a12 = ι+1 : 3ι-1, a11 = 1 : a12-1
@@ -82,11 +116,19 @@ function get_gorenstein_coefficients(ι :: T) where {T <: Integer}
 
                 A = SMatrix{4,2,T}(a11,a21,a31,a41,a12,a22,a32,a42)
                 all(a -> a > 0, A) || continue
-                push!(res, A)
+                push!(res, gorenstein_coefficients_normal_form(A))
                 
             end
         end
     end
+
+    return res
+
+end
+
+function classify_gorenstein_coefficients(ι :: T, ::Val{2}) where {T <: Integer}
+
+    res = Set{SMatrix{4,2,T}}()
 
     # Case a11 = a12 (hence also a31 = a32)
     for a11 = ι+1 : 3ι-1
@@ -109,9 +151,24 @@ function get_gorenstein_coefficients(ι :: T) where {T <: Integer}
 
             A = SMatrix{4,2,T}(a11,a21,a31,a41,a11,a22,a31,a42)
             all(a -> a > 0, A) || continue
-            push!(res, A)
+            push!(res, gorenstein_coefficients_normal_form(A))
 
         end
+    end
+
+    return res
+
+end
+
+function classify_gorenstein_coefficients(ι :: T, ::Val{3}) where {T <: Integer}
+
+    res = Set{SMatrix{4,2,T}}()
+
+    # Case a11 = a12 (hence also a31 = a32)
+    for a11 = ι+1 : 3ι-1
+        a31, a31rem = divrem(ι*a11, a11-ι)
+        a31rem == 0 || continue
+        a11 ≤ a31 || continue
 
         # Subcase a21 < a22.
         # Then min(a21,a42) < 2ι. By symmetry, we may assume
@@ -131,10 +188,30 @@ function get_gorenstein_coefficients(ι :: T) where {T <: Integer}
 
                 A = SMatrix{4,2,T}(a11,a21,a31,a41,a11,a22,a31,a42)
                 all(a -> a > 0, A) || continue
-                push!(res, A)
+                push!(res, gorenstein_coefficients_normal_form(A))
 
             end
+        end
+    end
 
+    return res
+
+end
+
+function classify_gorenstein_coefficients(ι :: T, ::Val{4}) where {T <: Integer}
+
+    res = Set{SMatrix{4,2,T}}()
+
+    # Case a11 = a12 (hence also a31 = a32)
+    for a11 = ι+1 : 3ι-1
+        a31, a31rem = divrem(ι*a11, a11-ι)
+        a31rem == 0 || continue
+        a11 ≤ a31 || continue
+
+        # Subcase a21 < a22.
+        # Then min(a21,a42) < 2ι. By symmetry, we may assume
+        # a21 = min(a21,a42) < 2ι.
+        for a21 = 1 : 2ι-1
             # Subsubcase a31 < a22. Then also a42 < a11
             for a42 = 1 : a11-1
                 a22, a22rem = divrem(a31*(a11*a21+a11*a42-a21*a42), a11*a42)
@@ -150,22 +227,29 @@ function get_gorenstein_coefficients(ι :: T) where {T <: Integer}
 
                 A = SMatrix{4,2,T}(a11,a21,a31,a41,a11,a22,a31,a42)
                 all(a -> a > 0, A) || continue
-                push!(res, A)
+                push!(res, gorenstein_coefficients_normal_form(A))
             end
         end
 
     end
 
     return res
-
 end
 
+classify_gorenstein_coefficients(ι :: T, ::Val{0}) where {T <: Integer} =
+union!(classify_gorenstein_coefficients(ι, Val(1)),
+       classify_gorenstein_coefficients(ι, Val(2)),
+       classify_gorenstein_coefficients(ι, Val(3)),
+       classify_gorenstein_coefficients(ι, Val(4)))
 
-function get_degree_matrix_minors(ι :: T) where {T <: Integer}
+classify_gorenstein_coefficients(ι :: Integer) =
+classify_gorenstein_coefficients(ι, Val(0))
+
+function classify_degree_matrix_minors(ι :: T, ::Val{t}) where {t, T <: Integer}
 
     res = SVector{6,T}[]
 
-    for A ∈ get_gorenstein_coefficients(ι)
+    for A ∈ classify_gorenstein_coefficients(ι, Val(t))
         m12, m13, m14, m23, m24, m34 = gorenstein_coefficients_to_degree_matrix_minors(ι,A)
 
         # check necessary condition for almost freeness
@@ -181,15 +265,17 @@ function get_degree_matrix_minors(ι :: T) where {T <: Integer}
 
 end
 
+classify_degree_matrix_minors(ι :: Integer) =
+classify_degree_matrix_minors(ι, Val(0))
+
+
 divisors(n :: Integer) = filter(k -> n % k == 0, 1 : abs(n))
 
-# Returns all toric surface with given prescribed gorenstein index
-# and free part of the degree matrix.
-function classify_quadrilaterals_by_gorenstein_index(ι :: T) where {T <: Integer}
+function classify_quadrilaterals_by_gorenstein_index(ι :: T, ::Val{t}) where {t, T <: Integer}
 
     result = Set{RationalPolygon{T,4,8}}()
 
-    for (m12, m13, m14, m23, m24, m34) in get_degree_matrix_minors(ι)
+    for (m12, m13, m14, m23, m24, m34) in classify_degree_matrix_minors(ι, Val(t))
         
         for ι4 in divisors(ι)
             ι4*(m12 - m24 - m14) % m14 == 0 || continue
@@ -236,3 +322,23 @@ function classify_quadrilaterals_by_gorenstein_index(ι :: T) where {T <: Intege
 
     return result
 end
+
+@doc raw"""
+    classify_quadrilaterals_by_gorenstein_index(ι :: Integer)
+
+Return all LDP quadrilaterals with Gorenstein index ι.
+
+# Example
+
+There are 73725 distincet LDP quadrilaterals with Gorenstein index at most 50.
+
+```jldoctest
+julia> Pss = classify_quadrilaterals_by_gorenstein_index.(1:50);
+
+julia> sum(length.(Pss))
+73725
+```
+
+"""
+classify_quadrilaterals_by_gorenstein_index(ι :: Integer) =
+classify_quadrilaterals_by_gorenstein_index(ι, Val(0))
