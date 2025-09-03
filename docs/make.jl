bd11c37df2cc686d05bfc312b7a6ffdba3172d1f
Justus Springer <justusspringer@gmx.de>
2025-09-03 11:44:24 +0200

    make docs/make.jl take an argument
    Supports three options "html", "pdf" and "tex" output.

--- docs/make.jl
+++ docs/make.jl
@@ -1,21 +1,61 @@
+if length(ARGS) > 1
+    println("Usage: julia make.jl <format>")
+    println("Format can be 'html', 'pdf', or 'tex'.")
+    exit(1)
+end
+
+format = isempty(ARGS) ? "html" : ARGS[1]
+
+if format ∉ ["html", "pdf", "tex"]
+    println("Usage: julia make.jl <format>")
+    println("Format can be 'html', 'pdf', or 'tex'.")
+    exit(1)
+end
+
+@info "make.jl: Building documentation for format \"$format\""
+
 using Documenter, DocumenterCitations, RationalPolygons, StaticArrays
 
 bib = CitationBibliography(joinpath(@__DIR__, "src", "refs.bib"))
 
-makedocs(
-    sitename = "RationalPolygons",
-    pages = [
-        "Home" => "index.md",
-        "2D Geometry" => "2dgeometry.md",
-        "Rational Polygons" => "polygons.md",
-        "LDP polygons and toric surfaces" => "ldp.md",
-        "Subpolygons" => "subpolygons.md",
-        "Classifications" => "classifications.md",
-        "Index" => "docs_index.md"
-    ],
-    plugins = [bib]
-)
+if format == "html"
+    makedocs(
+        sitename = "RationalPolygons",
+        pages = [
+            "Home" => "index.md",
+            "2D Geometry" => "2dgeometry.md",
+            "Rational Polygons" => "polygons.md",
+            "LDP polygons and toric surfaces" => "ldp.md",
+            "Subpolygons" => "subpolygons.md",
+            "Classifications" => "classifications.md",
+            "Index" => "docs_index.md"
+        ],
+        plugins = [bib]
+    )
 
-deploydocs(
-    repo = "github.com/justus-springer/RationalPolygons.jl.git",
-)
+    deploydocs(
+        repo = "github.com/justus-springer/RationalPolygons.jl.git",
+    )
+
+elseif format == "pdf"
+    makedocs(
+        sitename = "RationalPolygons",
+        format = Documenter.LaTeX(),
+        pages = [
+            "Home" => "index.md",
+            "2D Geometry" => "2dgeometry.md",
+            "Rational Polygons" => "polygons.md",
+            "LDP polygons and toric surfaces" => "ldp.md",
+            "Subpolygons" => "subpolygons.md",
+            "Classifications" => "classifications.md",
+            "Index" => "docs_index.md"
+        ],
+        plugins = [bib],
+    )
+
+elseif format == "tex"
+
+else
+    println("Invalid format. Please choose 'html', 'pdf', or 'tex'.")
+    exit(1)
+end
